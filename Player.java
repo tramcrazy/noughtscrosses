0bd20464fff9d55452580061c890d8b19196ee64
tramcrazy <tram@tramcrazy.com>
2024-10-13 18:22:17 +0100

    Add Player class

---
+++ Player.java
@@ -0,0 +1,8 @@
+public abstract class Player {
+    char playerSymbol;
+    String playerName;
+    public Player(char playerSymbol, String playerName) {
+        this.playerSymbol = playerSymbol;
+        this.playerName = playerName;
+    }
+}
